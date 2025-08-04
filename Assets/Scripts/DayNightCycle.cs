626c63a3c5313c6a2bf9d6e40cad97078b3f478f
nikmlnkr <nikmlnkr@gmail.com>
2025-08-04 20:29:35 +0530

    Update DayNightCycle.cs

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
+    public bool isDay = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // This is a minor change
     }
 
     // Update is called once per frame
